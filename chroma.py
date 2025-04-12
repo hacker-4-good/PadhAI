c353dc9c410bba35fdd10c76984d0d64fb1f9e1c
hacker-4-good <mayankhacker04@gmail.com>
2025-04-12 21:25:08 +0530

    update in code structure (more modular)

--- chroma.py
+++ chroma.py
@@ -1,11 +1,11 @@
 import chromadb 
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction 
 from langchain_community.document_loaders.pdf import PyPDFLoader
-import os
-import glob
+# import os
+# import glob
 
 
-client = chromadb.PersistentClient(path = "DB")
+client = chromadb.PersistentClient(path = "Database")
 
 collection = client.create_collection(
     name="Algorithm", 
@@ -13,12 +13,19 @@ collection = client.create_collection(
 )
 
 
-pdf_files = glob.glob("PDFs/*.pdf")
+# pdf_files = glob.glob("PDFs/*.pdf")
 
-for pdf_file in pdf_files:
-    documents = PyPDFLoader(file_path=pdf_file).load()
-    for i in range(len(documents)):
-        collection.add(
-            ids=f'{os.path.basename(pdf_file)}_id{i}', 
-            documents=documents[i].page_content
-        )
+# for pdf_file in pdf_files:
+#     documents = PyPDFLoader(file_path=pdf_file).load()
+#     for i in range(len(documents)):
+#         collection.add(
+#             ids=f'{os.path.basename(pdf_file)}_id{i}', 
+#             documents=documents[i].page_content
+#         )
+
+documents = PyPDFLoader(file_path="Introduction to Algorithm .pdf").load()
+for i in range(len(documents)):
+    collection.add(
+        ids=f'id{i}', 
+        documents=documents[i].page_content
+    )
