5a9328ab0360cad70164b9ea0bef61695bfa061e
Mayank Goswami <91790864+hacker-4-good@users.noreply.github.com>
2025-09-25 16:15:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import streamlit as st 
 
-ELEVEN_LABS_API_KEY = st.secrets["ELEVENLABS_API_KEY"]
+ELEVENLABS_API_KEY = st.secrets["ELEVENLABS_API_KEY"]
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 QDRANT_API_KEY = st.secrets["QDRANT_API_KEY"]
